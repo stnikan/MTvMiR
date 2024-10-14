afc42ee4cefa746b723112e1eff535dfdba2ea61
Stnikan <stnikan2016@gmail.com>
2024-10-14 17:07:09 +0300

    lab4 test init shim

--- lab4/main.c
+++ lab4/main.c
@@ -11,29 +11,30 @@ ISR(INT0_vect)
 {
     if ((PIND & (1 << 0)) != 0)
     {
-        EICRA = (1 << ISC01)  | (1 << ISC21);
+        EICRA = (1 << ISC01) | (1 << ISC21);
         if ((PIND & (1 << 1)) != 0)
-            angle+=5;
+            angle += 5;
         else
-            angle-=5;
+            angle -= 5;
     }
     else
     {
         EICRA = (1 << ISC01) | (1 << ISC00) | (1 << ISC21);
         if ((PIND & (1 << 1)) != 0)
-            angle-=5;
+            angle -= 5;
         else
-            angle+=5;
+            angle += 5;
     }
-    if (angle<0){angle = 0;}
-    
+    if (angle < 0)
+    {
+        angle = 0;
+    }
 }
 
-ISR(INT2_vect){angle=0;}
+ISR(INT2_vect) { angle = 0; }
 
 int main(void)
 {
-    
 
     DDRE = (1 << 3) | (1 << 4) | (1 << 5);
 
@@ -41,27 +42,22 @@ int main(void)
     EIMSK |= (1 << INT0);
     EIMSK |= (1 << INT2);
     EICRA = (1 << ISC01) | (1 << ISC00) | (1 << ISC21);
-    
-    init_ADC(1,ADEN);
+
+    init_ADC(1, ADEN);
     /* Инициализация таймера №3. 8-ми битная быстрая
     ШИМ, преддедитель на 8 */
-    //TCCR3A = (1 << COM3A1) | (1 << COM3B1) | (1 << COM3C1) | (1 << WGM30) | (1 << WGM31);
-    //TCCR3B = (1 << CS30)| (1 << WGM32);
-    init_shim_3(5,COM3A1,COM3B1,COM3C1,WGM30,WGM31);
-    //init_shim_3(2,CS30,WGM32);
-    
-    
-    
+    // TCCR3A = (1 << COM3A1) | (1 << COM3B1) | (1 << COM3C1) | (1 << WGM30) | (1 << WGM31);
+    // TCCR3B = (1 << CS30)| (1 << WGM32);
+    init_shim_3(5, 2, COM3A1, COM3B1, COM3C1, WGM30, WGM31, CS30, WGM32); // число настроек для TCCR3A и для TCCR3B, далее названия регистров
 
     // включение ацп
-    
+
+    sei();
 
     while (1)
     {
-        
-        OCR3AH = read_adc_10(3)>>8;
+
+        OCR3AH = read_adc_10(3) >> 8;
         OCR3AL = read_adc_10(3);
-        
-       
     }
 }
--- lab4/sourse/SHIM3.c
+++ lab4/sourse/SHIM3.c
@@ -2,41 +2,31 @@
 #include <util/delay.h> //для использования пауз
 #include <avr/interrupt.h>
 
-uint8_t my_find(uint8_t x){
-    uint8_t bit_A[] = {COM3A1,COM3A0,COM3B1,COM3B0, COM3C1,COM3C0, WGM31,WGM30};
-    uint8_t bit_B[] = {ICNC3,ICES3,WGM33, WGM32, CS32,CS31,CS30};
-    uint8_t flag = 0;
-    for (uint8_t i=0 ; i<8; i++){
-        if (bit_A[i]==x){flag = 1;break;}
-    }
-    return flag;
-}
 
-void init_shim_3(uint8_t num, uint8_t arg, ...){
+void init_shim_3(uint8_t numA, uint8_t numB, uint8_t arg, ...){
     TCCR3A = 0;
     TCCR3B = 0;
     uint8_t *p = &arg;
     //uint8_t bit_A[] = {COM3A1,COM3A0,COM3B1,COM3B0, COM3C1,COM3C0, WGM31,WGM30};
     //uint8_t bit_B[] = {ICNC3,ICES3,WGM33, WGM32, CS32,CS31,CS30};
-    while (num--)
+    while (numA--)
     {
-        /* code */
-        //if (my_find(*p)==1){TCCR3A|=(1<<*p);}
-        //else {TCCR3B|=1<<*p;}
-
-        TCCR3A|=(1<<*p);
-        
-        
+        TCCR3A|=(1<<*p);        
+        p++;
+    }
+    while (numB--)
+    {
+        TCCR3B|=(1<<*p);        
         p++;
     }
     //TCCR3A = (1 << COM3A1) | (1 << COM3B1) | (1 << COM3C1) | (1 << WGM30) | (1 << WGM31);
-    TCCR3B = (1 << CS30)| (1 << WGM32);
+    //TCCR3B = (1 << CS30)| (1 << WGM32);
 
-    if (TCCR3A==(1 << COM3A1) | (1 << COM3B1) | (1 << COM3C1) | (1 << WGM30) | (1 << WGM31)){
-        OCR3CH = 3;
-        OCR3CL=200;}
-    if (TCCR3B == (1 << CS30)| (1 << WGM32)){OCR3BH = 3;
-        OCR3BL=200;}
+    //if (TCCR3A==(1 << COM3A1) | (1 << COM3B1) | (1 << COM3C1) | (1 << WGM30) | (1 << WGM31)){
+    //    OCR3CH = 3;
+    //    OCR3CL=200;}
+    //if (TCCR3B == (1 << CS30)| (1 << WGM32)){OCR3BH = 3;
+    //    OCR3BL=200;}
     
     
 }
